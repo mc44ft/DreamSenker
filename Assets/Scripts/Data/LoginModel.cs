2888475a11485f859d8a3c5afc00dc8c0938b722
mc44ftFotMac <m18536729852@gmail.com>
2026-08-25 17:41:46 +0800

    接入登录认证流程
    - 新增登录数据模型与认证客户端\n- 更新登录面板预制体配置\n- 纳入相关 Unity 元数据文件

---
+++ Assets/Scripts/Data/LoginModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DreamSeeker.Data
+{
+    /// <summary>
+    /// 登录请求数据，对应服务端 LoginRequest。
+    /// </summary>
+    [Serializable]
+    public class LoginRequest
+    {
+        public string Username;//登录用户名
+        public string Password;//登录密码
+
+        public LoginRequest() { }
+
+        public LoginRequest(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+    }
+
+    /// <summary>
+    /// 登录响应数据，对应服务端 LoginResponse。
+    /// </summary>
+    [Serializable]
+    public class LoginResponse
+    {
+        public int UserId;//服务端用户唯一 ID
+        public string Username;//登录用户名
+        public string AccessToken;//访问令牌
+        public int ExpiresInSeconds;//访问令牌有效时间，单位为秒
+
+        public LoginResponse() { }
+    }
+}
---
+++ Assets/Scripts/Managers/AuthClient.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using DreamSeeker.Data;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace DreamSeeker.Managers
+{
+    public class AuthClient
+    {
+        private const string BaseUrl = "https://localhost:5210";
+
+        public IEnumerator Login(string username, string password,
+            Action<LoginResponse> onSuccess, Action<string> onFailure)
+        {
+            var data = new LoginRequest
+            {
+                Username = username,
+                Password = password,
+            };
+            string json = JsonUtility.ToJson(data);
+            byte[] body = Encoding.UTF8.GetBytes(json);
+
+            // 创建一个HTTP请求
+            using var request = new UnityWebRequest(
+                $"{BaseUrl}/api/auth/login", UnityWebRequest.kHttpVerbPOST);
+            
+            // 将body设置为请求的上传内容
+            request.uploadHandler = new UploadHandlerRaw(body);
+            // 为请求设置“响应数据接收器”。
+            // 服务器返回的数据会被暂存在内存缓冲区中，之后可以通过：
+            // request.downloadHandler.text
+            // 读取响应文本。
+            request.downloadHandler = new DownloadHandlerBuffer();
+            // 设置 HTTP 请求头，告诉服务器：
+
+            // 我发送的请求体内容是 JSON 格式。
+            request.SetRequestHeader("Content-Type", "application/json");
+
+            yield return request.SendWebRequest();
+
+            if (request.result == UnityWebRequest.Result.Success)
+            {
+                var response = JsonUtility.FromJson<LoginResponse>(request.downloadHandler.text);
+                onSuccess?.Invoke(response);
+            }
+            else
+            {
+                onFailure?.Invoke($"HTTP {request.responseCode}: {request.error}");
+            }
+        }
+    }
+}
